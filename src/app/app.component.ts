2f7dc0e1cb4c7e337ca3da14cb39c868b863ed90
Mairaj-M <mairaj.101010@gmail.com>
2026-01-08 00:11:40 +0530

    third commit and learned to call method on button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular19Learning';
+  title = 'My name is Mohd Miaraj';
+  name = 'Adil'
+
+  clickHandler(myVal:any){
+    console.log("Hi from button"+ myVal);
+  }
 }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : "app-profile",
+    imports :[],
+    // template : '<h1>Profile Componenet</h1>',
+    templateUrl : './profile.component.html',
+    // styles : 'h1{color : red}'
+    styleUrl : './profile.component.css'
+
+})
+
+export class ProfileComponent{
+
+}
